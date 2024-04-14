1821c9e6eb0baa6b8d65c53df7c3ed3fc009407a
LuizJMF <luizjmfilho@gmail.com>
2024-04-14 10:32:13 -0300

    Added the Google Play In-App Review API.

--- app/src/main/java/com/gmail/luizjmfilho/sevenwonders/MainActivity.kt
+++ app/src/main/java/com/gmail/luizjmfilho/sevenwonders/MainActivity.kt
@@ -5,9 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.view.WindowCompat
 import com.gmail.luizjmfilho.sevenwonders.ui.theme.SevenWondersTheme
+import com.google.android.play.core.review.ReviewManagerFactory
 import dagger.hilt.android.AndroidEntryPoint
+import hilt_aggregated_deps._dagger_hilt_android_internal_modules_ApplicationContextModule
 
 
 enum class ScreenNames {
@@ -28,6 +31,7 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        showFeedbackDialog()
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             val windowSizeClass = calculateWindowSizeClass(this)
@@ -35,6 +39,15 @@ class MainActivity : ComponentActivity() {
                 SevenWondersNavHost(
                     windowWidthSizeClass = windowSizeClass.widthSizeClass,
                 )
+            }
+        }
+    }
+
+    private fun showFeedbackDialog() {
+        val reviewManager = ReviewManagerFactory.create(applicationContext)
+        reviewManager.requestReviewFlow().addOnCompleteListener {
+            if(it.isSuccessful) {
+                reviewManager.launchReviewFlow(this, it.result)
             }
         }
     }
