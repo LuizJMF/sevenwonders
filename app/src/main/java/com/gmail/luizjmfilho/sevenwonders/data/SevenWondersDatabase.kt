1974d9638383c0cb4eb94fe51ada6ad3f9463696
Crístian Viana <cristiandeives@gmail.com>
2024-06-14 06:29:58 +0200

    Create database migration from version 3 to 4

--- app/src/main/java/com/gmail/luizjmfilho/sevenwonders/data/SevenWondersDatabase.kt
+++ app/src/main/java/com/gmail/luizjmfilho/sevenwonders/data/SevenWondersDatabase.kt
@@ -1,16 +1,24 @@
 package com.gmail.luizjmfilho.sevenwonders.data
 
+import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.RenameColumn
 import androidx.room.RoomDatabase
+import androidx.room.migration.AutoMigrationSpec
 import com.gmail.luizjmfilho.sevenwonders.model.Match
 import com.gmail.luizjmfilho.sevenwonders.model.Person
 
 @Database(
     entities = [Person::class, Match::class],
     version = 4,
-    exportSchema = true,
+    autoMigrations = [
+        AutoMigration(from = 3, to = 4, spec = AutoMigration3To4::class),
+    ]
 )
 abstract class SevenWondersDatabase : RoomDatabase() {
     abstract fun personDao(): PersonDao
     abstract fun matchDao(): MatchDao
 }
+
+@RenameColumn(tableName = "Person", fromColumnName = "nickname", toColumnName = "name")
+class AutoMigration3To4 : AutoMigrationSpec
